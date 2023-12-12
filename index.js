bd596641dbc6286c09bc1a1ea3a77e19ca68398c
Nathan-Simonnet <nathan.simonnet@gmail.com>
2023-12-12 14:42:30 +0100

    ready to JS

---
+++ index.js
@@ -0,0 +1,33 @@
+const toolContainer = document.querySelector('.tools-container');
+const colorsContainer = document.querySelector('.colors-container');
+const blackSpan = document.getElementById('black');
+const blueSpan = document.getElementById('blue');
+const greenSpan = document.getElementById('green');
+const orangeSpan = document.getElementById('orange');
+const redSpan = document.getElementById('red');
+const rangeContainer = document.querySelector('.range-container');
+const range = document.querySelector('input type="range');
+const canva = document.querySelector('canvas');
+const eraseContainer = document.querySelector('.erase-container');
+const erase = document.querySelector('erase');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
